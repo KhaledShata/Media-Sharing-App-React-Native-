800509fcbc5961e33f3b7e0904ece3a5359b4a2f
Khaled Atef <khaledatef7070@gmail.com>
2024-04-16 17:05:13 +0200

    Upload working to same MONGODB

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import axios from 'axios';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from "react-native";
+import {launchImageLibrary} from 'react-native-image-picker';
+import * as ImagePicker from 'expo-image-picker';
 
-export default function App() {
+const options = {
+  title: 'Select Image',
+  type:'Library', 
+  options:{
+    mediaType: 'mixed',
+    selectionLimit: 1,
+    includeBase64: false,
+    
+  }
+
+};
+
+
+const App = () =>{
+
+    const handleUpload=async()=>{
+      console.log("HIIII");
+      const file = await ImagePicker.launchImageLibraryAsync(options);
+      console.log(file.assets[0]);
+      const formData = new FormData();
+      formData.append('file', {
+        uri:file.assets[0].uri,
+        type:file.assets[0].mimeType,
+        name:file.assets[0].fileName,
+      })
+      let res = await fetch(
+        'http://192.168.100.4:3001/upload', 
+        {
+          method :'post', 
+          body:formData, 
+          headers:{
+          'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
+      let responsejson = await res.json();
+      console.log("RESPONSE IS " ,responsejson);
+
+    //   axios.post('http://localhost:3001/upload', formData, {
+    //   headers: {
+    //     'Content-Type': 'multipart/form-data',
+    //   },
+    // })
+    console.log("UPLOAD DONE SUCCESSFULLY");
+
+    }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Upload" onPress={handleUpload} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+export default App;
