81f7f6040acd1802d0e23782c13b07d41f197a40
Khaled Atef <khaledatef7070@gmail.com>
2024-04-17 03:02:33 +0200

    Reading Done - without buttons

--- App.js
+++ App.js
@@ -1,38 +1,25 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import axios from 'axios';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-} from "react-native";
+import {  StyleSheet,  Text,  View,  Button,ScrollView,Image} from "react-native";
 import {launchImageLibrary} from 'react-native-image-picker';
 import * as ImagePicker from 'expo-image-picker';
-
-const options = {
-  title: 'Select Image',
-  type:'Library', 
-  options:{
-    mediaType: 'all',
-    selectionLimit: 1,
-    includeBase64: false,
-    
-  }
-
-};
+import { Video } from 'expo-av'; 
 
 
 const App = () =>{
+  const [filesList, setFilesList] = useState([]);
+    useEffect(() => {
+      fetchFiles(); 
+    }, []); 
+
 
     const handleUpload=async()=>{
-      console.log("HIIII");
       const file = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.All,
         allowsEditing: false, //disable crop after selecting photo
         quality: 0.9,
         base64: false,
       });
-      console.log(file.assets[0]);
       const formData = new FormData();
       formData.append('file', {
         uri:file.assets[0].uri,
@@ -40,36 +27,101 @@ const App = () =>{
         name:file.assets[0].fileName,
       })
       console.log("URI" , file.assets[0].uri)
-      console.log("URI" , file.assets[0].mimeType)
-      console.log("URI" , file.assets[0].fileName)
+      console.log("Type" , file.assets[0].mimeType)
+      console.log("Name" , file.assets[0].fileName)
 
       let res = await fetch(
-        'http://192.168.100.4:3001/upload', 
+        'http://192.168.100.4:3001/upload', //local host raise connection failed
         {
           method :'post', 
           body:formData, 
-          // headers:{
-          // 'Content-Type': 'multipart/form-data',
-          // },
+          headers:{
+          'Content-Type': 'multipart/form-data',
+          
+          },
         }
       );
       let responsejson = await res.json();
       console.log("RESPONSE IS " ,responsejson);
-
-    //   axios.post('http://localhost:3001/upload', formData, {
-    //   headers: {
-    //     'Content-Type': 'multipart/form-data',
-    //   },
-    // })
-    console.log("UPLOAD DONE SUCCESSFULLY");
+      console.log("UPLOAD DONE");
 
     }
+
+    const fetchFiles = () => {
+      axios.get('http://192.168.100.4:3001/files')
+        .then(res => {
+          const files = res.data;
+          const detailFetchPromises = files.map(file =>
+            axios.get(`http://192.168.100.4:3001/media/${file.gridfsId}`)
+              .then(mediaResponse => {
+                const { _id, ...mediaDetails } = mediaResponse.data; 
+                return {
+                  ...file, 
+                  ...mediaDetails, 
+                };
+              })
+          );
+    
+          Promise.all(detailFetchPromises)
+            .then(fullFiles => {
+              setFilesList(fullFiles);
+              console.log("FILES FETCHED " , fullFiles[0]);
+            })
+            .catch(error => console.error('Error fetching media details:', error));
+        })
+        .catch(err => console.error('Error fetching files:', err)); 
+    };
+    const renderFile = (file) => {
+      const fileType = file.contentType.split('/')[0]; 
+      const fileSource = `http://192.168.100.4:3001/files/view/${file._id}?cb=${new Date().getTime()}`;
+
+      if (fileType === 'image') {
+        return (
+          <View style={styles.fileContainer}>
+          <Text style={styles.fileInfo}>{file.filename}</Text>  
+
+            <Image
+              source={{ uri: fileSource }}
+              style={styles.image}
+            />
+            <Text style={styles.fileInfo}>{file.numberofLikes} Likes</Text>
+          </View>
+        );
+      } else if (fileType === 'video') {
+        return (
+          <View style={styles.fileContainer}>
+            <Text style={styles.fileInfo}>{file.filename}</Text>  
+
+            <Video
+              source={{ uri: fileSource }}
+              style={styles.video}
+              useNativeControls
+              resizeMode="contain"
+              isLooping
+            />
+            <Text style={styles.fileInfo}>{file.numberofLikes} Likes</Text>
+          </View>
+        );
+      }
+    };
+
   
-  return (
-    <View style={styles.container}>
-      <Button title="Upload" onPress={handleUpload} />
-    </View>
-  );
+    return (
+      <View style={styles.container}>
+        <Text>Media Sharing Platform</Text>
+        <Button title="Upload" onPress={handleUpload} />
+
+      <ScrollView style={styles.filesList}>
+        {filesList.map((file, index) => (
+          <View key={index}>
+            {renderFile(file)}
+          </View>
+        ))}
+      </ScrollView>
+              
+      </View>
+    );
+    
 };
 
 const styles = StyleSheet.create({
@@ -77,6 +129,31 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  fileContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    borderColor: 'grey',  
+    borderWidth: 2,       
+    padding: 10,          
+    backgroundColor: '#fff', 
+  },
+  image:{
+      width: 300, 
+      height: 300,
+      resizeMode:'contain'
+
+  },
+  video:{
+      width: 300, 
+      height: 300,
+      resizeMode:'contain'
+  },
+  fileInfo: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8,
   },
 });
 export default App;
